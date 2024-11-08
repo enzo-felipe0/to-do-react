f75f3fe225a86a4358881bea35e013c8a99c7395
Enzo <enzofelipe2525@gmail.com>
2024-11-08 10:43:17 -0300

    Adicionando state lista e formulario

--- src/components/Formulario/Formulario.tsx
+++ src/components/Formulario/Formulario.tsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import { Button } from "../Buttons/Button"
 import style from './Formulario.module.scss'
 
 export function Formulario() {
+    const [tarefa, setTarefa] = useState("");
+    const [tempo, setTempo] = useState("00:00");
+
+    function adicionarTarefa(evento: React.FormEvent<HTMLFormElement>){
+        evento.preventDefault();
+    }
     return (
         <>
-            <form action="" className={style.novaTarefa}>
+            <form action="" className={style.novaTarefa} onSubmit={adicionarTarefa}>
                 <div className={style.inputContainer}>
                     <label htmlFor="tarefa">
                         Adiciona um novo estudo
@@ -12,6 +19,8 @@ export function Formulario() {
                     <input
                         type="text"
                         name="tarefa"
+                        value={tarefa}
+                        onChange={evento => setTarefa(evento.target.value)}
                         id="tarefa"
                         placeholder="O que você quer estudar?"
                         required
@@ -25,6 +34,8 @@ export function Formulario() {
                         type="time"
                         step="1"
                         name="tempo"
+                        value={tempo}
+                        onChange={evento => setTempo(evento.target.value)}
                         id="tempo"
                         min="00:00:00"
                         max="01:30:00"
--- src/components/Lista/Lista.tsx
+++ src/components/Lista/Lista.tsx
@@ -1,8 +1,9 @@
- import { Item } from './Item/Item';
+ import { useState } from 'react';
+import { Item } from './Item/Item';
 import style from './Lista.module.scss'
 
 export function Lista() {
-    const tarefas = [{
+    const [tarefas, setTarefas] = useState([{
         tarefa: 'React',
         tempo: '02:00:00',
     }, {
@@ -11,11 +12,13 @@ export function Lista() {
     }, {
         tarefa: 'TypeScript',
         tempo: '03:00:00'
-    }];
+    }]); 
     return (
         <>
             <aside className={style.listaTarefas}>
-                <h2>Estudos do dia</h2>
+                <h2 onClick={() => {
+                    setTarefas([...tarefas, { tarefa: "Estudar estado", tempo: "05:00:00"}]);
+                }}>Estudos do dia</h2>
                 <ul>
                     {tarefas.map((item, index) => (
                         <Item 
